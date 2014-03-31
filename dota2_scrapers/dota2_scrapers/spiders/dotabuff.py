c10d7d70852ac4c3e8254cb0420f91e255de7fdc
Tiffany Wang <tinnywang@gmail.com>
2014-03-31 16:42:11 -0700

    scrapy crawler for heroes on dotabuff

---
+++ dota2_scrapers/dota2_scrapers/spiders/dotabuff.py
@@ -0,0 +1,27 @@
+from scrapy.item import Item, Field
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import Selector
+
+class Hero(Item):
+    name = Field(serializer=str)
+    win_rate = Field(serializer=lambda x: float(x.replace('%', '')))
+    matches_played = Field(serializer=lambda x: int(x.replace(',', '')))
+    kda_ratio = Field(serializer=lambda x: float(x.replace('%', '')))
+
+class DotabuffSpider(CrawlSpider):
+    name = 'dotabuff'
+    start_urls = ['http://dotabuff.com']
+    rules = [Rule(SgmlLinkExtractor(allow='heroes\/winning\?date='), 'parse_hero')]
+
+    def parse_hero(self, response):
+        heroes = []
+        sel = Selector(response)
+        for row in sel.xpath('//table//tbody//tr'):
+            hero = Hero()
+            hero['name'] = row.xpath('.//td[2]//a[@class="hero-link"]/text()').extract()[0]
+            hero['win_rate'] = row.xpath('.//td[3]//div[1]/text()').extract()[0]
+            hero['matches_played'] = row.xpath('.//td[4]//div[1]/text()').extract()[0]
+            hero['kda_ratio'] = row.xpath('.//td[5]//div[1]/text()').extract()[0]
+            heroes.append(hero)
+        return heroes
