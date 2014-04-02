153cbed7ba3e92d8690598baa6fc6702fbe3692e
Tiffany Wang <tinnywang@gmail.com>
2014-04-01 22:30:28 -0700

    dotabuff heroes and players can be scraped

--- dota2_scrapers/dota2_scrapers/pipelines.py
+++ dota2_scrapers/dota2_scrapers/pipelines.py
@@ -10,9 +10,12 @@ class Dota2ScrapersPipeline(object):
     def process_item(self, item, spider):
         return item
 
+def item_type(item):
+    return type(item).__name__.lower()
+
 class DotabuffCsvItemPipeline(object):
     def __init__(self):
-        self.files = {}
+        self.item_types = ['hero', 'player']
 
     @classmethod
     def from_crawler(cls, crawler):
@@ -22,16 +25,16 @@ class DotabuffCsvItemPipeline(object):
         return pipeline
 
     def spider_opened(self, spider):
-        file = open('%s_heroes.csv' % spider.name, 'w+b')
-        self.files[spider] = file
-        self.exporter = CsvItemExporter(file)
-        self.exporter.start_exporting()
+        self.files = dict([(item_type, open('%s_%s.csv' % (spider.name, item_type), 'w+b')) for item_type in self.item_types])
+        self.exporters = dict([(item_type, CsvItemExporter(self.files[item_type])) for item_type in self.item_types])
+        [e.start_exporting() for e in self.exporters.values()]
 
     def spider_closed(self, spider):
-        self.exporter.finish_exporting()
-        file = self.files.pop(spider)
-        file.close()
+        [e.finish_exporting() for e in self.exporters.values()]
+        [f.close() for f in self.files.values()]
 
     def process_item(self, item, spider):
-        self.exporter.export_item(item)
+        t = item_type(item)
+        if t in self.item_types:
+            self.exporters[t].export_item(item)
         return item
--- dota2_scrapers/dota2_scrapers/spiders/dotabuff.py
+++ dota2_scrapers/dota2_scrapers/spiders/dotabuff.py
@@ -1,7 +1,7 @@
 from scrapy.item import Item, Field
-from scrapy.contrib.spiders import CrawlSpider, Rule
-from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.spider import Spider
 from scrapy.selector import Selector
+import re
 
 class Hero(Item):
     name = Field(serializer=str)
@@ -9,11 +9,18 @@ class Hero(Item):
     matches_played = Field(serializer=lambda x: int(x.replace(',', '')))
     kda_ratio = Field(serializer=lambda x: float(x))
 
-class DotabuffSpider(CrawlSpider):
-    name = 'dotabuff'
-    start_urls = ['http://dotabuff.com/heroes']
-    rules = [Rule(SgmlLinkExtractor(allow='winning'), 'parse_hero')]
+class Player(Item):
+    player = Field(serializer=str)
+    matches_played = Field(serializer=lambda x: int(x.replace(',', '')))
+    win_rate = Field(serializer=lambda x: float(x.replace('%', '')))
 
+class DotabuffSpider(Spider):
+    name = 'dotabuff'
+    start_urls = [
+        'http://dotabuff.com/heroes/winning?date=',
+        'http://dotabuff.com/players/verified'
+        ]
+    
     def parse_hero(self, response):
         heroes = []
         sel = Selector(response)
@@ -25,3 +32,20 @@ class DotabuffSpider(CrawlSpider):
             hero['kda_ratio'] = row.xpath('.//td[5]//div[1]/text()').extract()[0]
             heroes.append(hero)
         return heroes
+
+    def parse_player(self, response):
+        players = []
+        sel = Selector(response)
+        for row in sel.xpath('//table//tbody//tr'):
+            player = Player()
+            player['player'] = row.xpath('.//td[2]//a/text()').extract()[0]
+            player['matches_played'] = row.xpath('.//td[3]//div[1]/text()').extract()[0]
+            player['win_rate'] = row.xpath('.//td[4]//div[1]/text()').extract()[0]
+            players.append(player)
+        return players
+
+    def parse(self, response):
+        if re.search('heroes\/winning', response.url):
+            return self.parse_hero(response)
+        elif re.search('players\/verified', response.url):
+            return self.parse_player(response)
